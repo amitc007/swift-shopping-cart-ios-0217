1e0da5e33c824346726341c7af1deabc573f829f
ac <ac@Jaxxs-MBP.home>
2017-01-26 12:47:17 -0500

    completed

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,6 +7,64 @@
 //
 
 class Cart {
+    var items:[Item] = []
     
+    //1
+    func totalPriceInCents()->Int {
+        var totalPrice = 0
+        for itm in items {
+            totalPrice += itm.priceInCents
+        }
+        return totalPrice
+    }
     
+    //2
+    func add(item:Item) {
+        items.append(item)
+    }
+    
+    //3
+    func remove(item:Item) {
+        for (idx,itm) in items.enumerated() {
+            if itm == item {
+                items.remove(at: idx)
+                break
+            }
+        }
+        
+    }
+    
+    //4
+    func items(withName name:String)->[Item] {
+        var sameNameItm:[Item] = []
+        for itm in items {
+            if itm.name == name {
+                sameNameItm.append(itm)
+            }
+            
+        }
+        return sameNameItm
+    }
+    
+    //5
+    func items(withMinPrice price:Int)->[Item] {
+        var samePriceItems:[Item] = []
+        for itm in items {
+            if itm.priceInCents >= price {
+                samePriceItems.append(itm)
+            }
+        }
+        return samePriceItems
+    }
+    
+    //6
+    func items(withMaxPrice price:Int)->[Item] {
+        var itemsList:[Item] = []
+        for itm in items {
+            if itm.priceInCents <= price {
+                itemsList.append(itm)
+            }
+        }
+        return itemsList
+    }
 }
